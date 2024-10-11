fe4079af6f6b0898ba953a46b009752217eea690
Coreandrum1 <core_andrum769@hotmail.com>
2024-10-11 14:47:49 -0700

    typo

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Francisco Sabino Lopez Escamilla</h1>
+      <h1>Francisco Sabino López Escamilla</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
